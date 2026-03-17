64fbce9755f5b5fb836cc1e242d0c9becae204c6
Eduard Lupsa <eduard.lupsa@student.unitbv.ro>
2026-03-17 14:14:55 +0200

    Adaugare fisier de tranzactie cu serverul.

---
+++ server_transaction.sv
@@ -0,0 +1,18 @@
+class server_transaction;
+	
+	rand bit [31:0] data;
+	
+	function void post_randomize();
+		$display("--------- [Server Trans] post_randomize ------");
+		$display("Write data: %b", data);
+		$display("----------------------------------------------");
+	endfunction
+	
+	function server_transaction do_copy();
+		server_transaction server_trans;
+		server_trans = new();
+		server_trans.data = this.data;		
+	endfunction
+	
+	
+endclass
