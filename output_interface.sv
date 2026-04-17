203a72b82e19bb832319f3056c270246f0ac9b00
Bianca Pisău <148549019+Biancaa19@users.noreply.github.com>
2026-04-17 10:46:04 +0300

    modificare interfata iesire

--- output_interface.sv
+++ output_interface.sv
@@ -4,10 +4,12 @@ interface output_interface(input logic clk_i, rst_ni);
   logic [9:0]  yaw_pos_o;    // 10 biți: orientare nacelă
   logic        heat_o;       // 1 bit: on/off încălzire
   logic        em_brake_o;   // 1 bit: on/off frână urgență
+  logic [3:0]  error_feedback_o; // 4 biți: erori rotire, nacelă, încălzire
+
 
   clocking monitor_cb @(posedge clk_i);
     default input #1 output #1;
-    input blade_pos_o, yaw_pos_o, heat_o, em_brake_o;
+    input blade_pos_o, yaw_pos_o, heat_o, em_brake_o, error_feedback_o;
   endclocking
 
   modport MONITOR (clocking monitor_cb, input clk_i, rst_ni);
