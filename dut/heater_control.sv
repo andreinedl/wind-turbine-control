b85218a956647493a5a920ff9479bb3ac72338c9
Andrei Nedelcu <andreinedelcu48@gmail.com>
2026-04-26 22:35:44 +0300

    Refactorizare modul incalzire auxiliara

--- dut/heater_control.sv
+++ dut/heater_control.sv
@@ -1,58 +1,63 @@
 module heater_control #(
-    // Parametri pentru timp (50MHz * 300 secunde)
-    // 50.000.000 * 300 = 15.000.000.000
-    // ATENȚIE: Această valoare depășește 32 de biți, avem nevoie de 34 biți
-    parameter logic [33:0] FIVE_MIN_TICKS = 34'd15_000_000_000
+    parameter HEAT_ON_TSH = 30, // 5 grade Celsius
+    parameter HEAT_OFF_TSH = 35, // 10 grade Celsius
+    parameter HEAT_ERR_CNT_TSH = 1000
 )
 (
-    input  logic       clk_i,			// 50MHz
-    input  logic       rst_ni,
-    input  logic [6:0] temp_value_i,	// 0=-25C, 100=75C (Offset 25)
+    input  logic            clk_i,			
+    input  logic            rst_ni,
+    input  logic [7 - 1:0]  temp_value_i,	// 0=-25C, 100=75C (Offset 25)
     
-    output logic       heat_o,			// Comandă rezistență (1 = ON)
-    output logic       error			// Eroare dacă temp nu crește în 5 min
+    output logic            heat_o,			// Comandă rezistență (1 = ON)
+    output logic            error_o
 );
 
-    logic [33:0] timer;
-    logic [6:0]  prev_temp;
-    logic        temp_stable;
+logic                                   heat_err_cnt_tick;
+logic [$clog2(HEAT_ERR_CNT_TSH) - 1:0]  heat_err_cnt;
+logic [7 - 1:0]                         prev_temp;
+logic                                   heat_d;
+logic                                   heat_tick;
 
-    // 1. Logica de control a încălzirii
-    // Pornim dacă temp scade sub 5°C (valoarea 30)
-    // Oprim dacă temp depășește 10°C (valoarea 35) - Histerezis
-    always_comb begin
-        if (temp_value_i < 7'd30)
-            heat_o = 1'b1;
-        else if (temp_value_i > 7'd35)
-            heat_o = 1'b0;
-        else
-            heat_o = heat_o; // Menține starea anterioară (necesită logică secvențială pentru latch-less, dar aici lăsăm simplu)
+assign heat_err_cnt_tick = (heat_err_cnt == HEAT_ERR_CNT_TSH - 1);
+
+// Control incalzire
+always_ff @(posedge clk_i or negedge rst_ni) begin
+    if (!rst_ni)                        heat_o <= 1'b0; else // la reset dorim ca incalzirea auxiliara sa fie dezactivata
+    if (error_o)                        heat_o <= 1'b0; else // vrem sa oprim incalzirea daca avem o eroare
+    if (temp_value_i > HEAT_OFF_TSH)    heat_o <= 1'b0; else
+    if (temp_value_i < HEAT_ON_TSH)     heat_o <= 1'b1;
+end
+
+//Detector de front pentru heat: detectam momentul in care s-a pornit incalzirea
+assign heat_tick = ~heat_d & heat_o;
+always_ff @(posedge clk_i or negedge rst_ni) begin
+    if(!rst_ni) heat_d <= 1'b0; else
+                heat_d <= heat_o;
+end
+
+// Monitorizare incalzire / trimitere eroare
+// Verificam daca a crescut temperatura doar dupa un anumit timp setat, daca nu a crescut, returnam o eroare
+always_ff @(posedge clk_i or negedge rst_ni) begin
+    if (!rst_ni)                                    heat_err_cnt <= '0; 
+    else if(heat_o) begin
+        if (heat_err_cnt == HEAT_ERR_CNT_TSH - 1)   heat_err_cnt <= '0; else
+                                                    heat_err_cnt <= heat_err_cnt + 1;
     end
+    else if(!heat_o)                                heat_err_cnt <= '0;
+end
 
-    // 2. Monitorizarea eficienței încălzirii
-    // Dacă heat_o este pornit, temperatura trebuie să crească
-    assign temp_stable = (temp_value_i <= prev_temp);
+// Salvam temperatura din momentul in care incalzirea auxiliara este activata
+always_ff @(posedge clk_i or negedge rst_ni) begin
+    if (!rst_ni)                    prev_temp <= '0;
+    else if(heat_tick)              prev_temp <= temp_value_i;
+end
 
-    always_ff @(posedge clk_i or negedge rst_ni) begin
-        if (!rst_ni) begin
-            timer     <= 34'd0;
-            error   <= 1'b0;
-            prev_temp <= 7'd0;
-        end else begin
-            // Salvăm temperatura anterioară la intervale regulate (ex: la fiecare secundă) 
-            // sau comparăm direct pentru timeout
-            if (heat_o && temp_stable) begin
-                if (timer < FIVE_MIN_TICKS) begin
-                    timer <= timer + 1;
-                end else begin
-                    error <= 1'b1; // Rezistența merge de 5 min și temp nu a crescut
-                end
-            end else begin
-                timer   <= 34'd0;
-                error <= 1'b0;
-                prev_temp <= temp_value_i;
-            end
-        end
+always_ff @(posedge clk_i or negedge rst_ni) begin
+    if (!rst_ni)                        error_o <= 1'b0;
+    else if(heat_o & heat_err_cnt_tick) begin
+        if(temp_value_i <= prev_temp)   error_o <= 1'b1; else
+                                        error_o <= 1'b0;
     end
+end
 
 endmodule
