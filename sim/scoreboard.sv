52d5e0d6accb6913a3dcce2eeb8900a3304c7f1c
Andrei Nedelcu <andreinedelcu48@gmail.com>
2026-05-03 11:36:35 +0300

    Adaugare verificari in scoreboard pentru pozitia nacelei, unghiul palelor si frana de urgenta

--- sim/scoreboard.sv
+++ sim/scoreboard.sv
@@ -2,8 +2,13 @@ class scoreboard;
     // praguri/limite
     localparam HEAT_EN_TEMP = 30; // 5 grade
     localparam HEAT_DIS_TEMP = 35; // 10 grade
+    localparam YAW_MAX_POS = 720;
+    localparam MAX_WIND	   = 250;
+    localparam MAX_RPM     = 350; //35 RPM
+    localparam WIND_ANGLE_INCREASE_TSH = 120;
 
     bit expected_heat_state = 0; // variabila de stare pentru temperatura
+    bit yaw_last_pos = 0;        // variabila de stare pentru pozitia nacelei
 
     // mailbox-uri
     mailbox input_mon2scb;
@@ -29,7 +34,7 @@ class scoreboard;
     endfunction
 
     function pass(string signal_name, int value);
-        $error("[SCB-PASS] %s :: Expected = %d - Actual = %d", signal_name, value, value);
+        $display("[SCB-PASS] %s :: Expected = %d - Actual = %d", signal_name, value, value);
         pass_cnt++;
     endfunction
 
@@ -49,20 +54,85 @@ class scoreboard;
             // verificare incalzire auxiliara turbina
             if(input_tr.temp_value_i < HEAT_EN_TEMP) begin
                 expected_heat_state = 1;
-                if(output_tr.heat_o) pass("heat_o", expected_heat_state);
-                else                 err("heat_o", expected_heat_state, output_tr.heat_o);
-
-            end else if(input_tr.temp_value_i > HEAT_DIS_TEMP) begin
+                if(output_tr.heat_o) 
+                    pass("heat_o", expected_heat_state);
+                else                 
+                    err("heat_o", expected_heat_state, output_tr.heat_o);
+            end 
+            else if(input_tr.temp_value_i > HEAT_DIS_TEMP) begin
                 expected_heat_state = 0;
-                if(!output_tr.heat_o) pass("heat_o", expected_heat_state);
-                else                  err("heat_o", expected_heat_state, output_tr.heat_o);
-
-            end else begin
+                if(!output_tr.heat_o) 
+                    pass("heat_o", expected_heat_state);
+                else                  
+                    err("heat_o", expected_heat_state, output_tr.heat_o);
+            end 
+            else begin
                 if(output_tr.heat_o == expected_heat_state) 
                     pass("heat_o", expected_heat_state);
                 else 
                     err("heat_o", expected_heat_state, output_tr.heat_o);
             end
+
+            // verificare comanda nacela
+            if(input_tr.wind_dir_i > YAW_MAX_POS) begin
+                if(output_tr.yaw_pos_o == yaw_last_pos) 
+                    pass("yaw_pos_o", yaw_last_pos);      
+                else 
+                    err("yaw_pos_o", yaw_last_pos, output_tr.yaw_pos_o);
+            end 
+            else begin
+                yaw_last_pos = input_tr.wind_dir_i;
+                if(output_tr.yaw_pos_o == input_tr.wind_dir_i) 
+                    pass("yaw_pos_o", input_tr.wind_dir_i);
+                else 
+                    err("yaw_pos_o", input_tr.wind_dir_i, output_tr.yaw_pos_o);
+            end
+
+            // verificam franarea de urgenta
+            if(input_tr.rpm_value_i >= MAX_RPM) begin
+                if(output_tr.em_break_o) 
+                    pass("em_break_o", 1);
+                else                          
+                    err("em_break_o", 1, output_tr.em_break_o);
+            end 
+            else begin
+                if(!output_tr.em_break_o) 
+                    pass("em_break_o", 0);
+                else                          
+                    err("em_break_o", 0, output_tr.em_break_o);
+            end
+
+            // verificare comanda pozitie pale
+            if(input_tr.rpm_value_i >= MAX_RPM || input_tr.wind_speed_i > MAX_WIND) begin
+                // verificam daca palele s-au dus la pozitia de 90 de grade
+                if(output_tr.blade_pos_o == 180) 
+                    pass("blade_pos_o", 180);
+                else                             
+                    err("blade_pos_o", 180, output_tr.blade_pos_o);
+            end 
+            else if(input_tr.wind_speed_i > WIND_ANGLE_INCREASE_TSH) begin
+                int correct_value = (input_tr.wind_speed_i - WIND_ANGLE_INCREASE_TSH) / 2;
+                //verificam pozitia palelor
+                if(output_tr.blade_pos_o == correct_value)
+                    pass("blade_pos_o", output_tr.blade_pos_o);
+                else
+                    err("blade_pos_o", correct_value, output_tr.blade_pos_o);
+            end
+            else begin
+                //verificam pozitia palelor
+                if(output_tr.blade_pos_o == 0)
+                    pass("blade_pos_o", output_tr.blade_pos_o);
+                else
+                    err("blade_pos_o", 0, output_tr.blade_pos_o);
+            end
+
+            // verificare semnal error_feedback
+            /*if(output_tr.em_brake_o) begin
+                if(output_tr.error_feedback_o[3]) 
+                    pass("error_feedback_o[3]", 1);
+                else
+                    err("error_feedback_o[3]", 0, output_tr.error_feedback_o);
+            end*/
 
         end
     endtask
